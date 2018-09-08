db4f5c0a53a048ebf0e9c27c52766a7eb3acb5d2
silvercomtech <deskey@silvercomtech.com>
2018-09-08 14:21:23 -0400

    new edits

---
+++ src/test/java/silvercomtech/web/bean/feedback.java
@@ -0,0 +1,13 @@
+package silvercomtech.web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "feedback")
+@ViewScoped
+public class feedback {
+	
+	private Integer rating; 
+	
+	
+}
